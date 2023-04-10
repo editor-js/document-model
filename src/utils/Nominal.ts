1c4e180fb12aa480af78c4ac1b0eef6b98b240dd
Ilya Maroz <moroz.ilya.olegovich@gmail.com>
2023-04-10 23:20:30 +0100

    docs(nominal-types): add type for Nominal alias

--- src/utils/Nominal.ts
+++ src/utils/Nominal.ts
@@ -1,3 +1,6 @@
 declare const nominalTypeField: unique symbol;
 
+/**
+ * An alias for creating a nominal type
+ */
 export type Nominal<Type, Identifier> = Type & { readonly [nominalTypeField]: Identifier };
