7393d74740460fcb657c5af5ec93d7a21ad8812c
Ilya Maroz <moroz.ilya.olegovich@gmail.com>
2023-04-16 20:05:58 +0100

    docs(nominal-types): add descriptions for nominal types used for name fields etc

--- src/entities/BlockNode/types/DataKey.ts
+++ src/entities/BlockNode/types/DataKey.ts
@@ -1,5 +1,16 @@
 import { create, Nominal } from '../../../utils/Nominal';
 
-export type DataKey = Nominal<string, 'DataKey'>;
+/**
+ * Base type of the data key field
+ */
+type DataKeyBase = string;
 
+/**
+ * Nominal type for the data key field
+ */
+export type DataKey = Nominal<DataKeyBase, 'DataKey'>;
+
+/**
+ * Function returns a value with the nominal DataKey type
+ */
 export const createDataKey = create<DataKey>();
--- src/entities/BlockTune/types/BlockTuneName.ts
+++ src/entities/BlockTune/types/BlockTuneName.ts
@@ -1,7 +1,16 @@
 import { Nominal, create } from '../../../utils/Nominal';
 
+/**
+ * Base type of the block tune name field
+ */
 type BlockTuneNameBase = string;
 
+/**
+ * Nominal type for the block tune name field
+ */
 export type BlockTuneName = Nominal<BlockTuneNameBase, 'BlockTuneName'>;
 
+/**
+ * Function returns a value with the nominal BlockTuneName type
+ */
 export const createBlockTuneName = create<BlockTuneName>();
