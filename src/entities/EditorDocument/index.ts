61a7669d38785e8d3f93e1fd2f1a061ce6b7ba31
Ilya Maroz <moroz.ilya.olegovich@gmail.com>
2023-05-24 20:27:13 +0100

    feat(editor-document): implement addBlock method

--- src/entities/EditorDocument/index.ts
+++ src/entities/EditorDocument/index.ts
@@ -27,4 +27,35 @@ export class EditorDocument {
     this.#children = children;
     this.#properties = properties;
   }
+
+  /**
+   * Adds a BlockNode to the EditorDocument at the specified index.
+   * If no index is provided, the BlockNode will be added to the end of the array.
+   * Throws an error if the index is out of bounds.
+   *
+   * @param blockNode - The BlockNode to add to the EditorDocument
+   * @param index - The index at which to add the BlockNode
+   */
+  public addBlock(blockNode: BlockNode, index?: number): void | never {
+    if (index === undefined) {
+      /**
+       * Always create a new array when adding a new block
+       */
+      this.#children = [...this.#children, blockNode];
+
+      return;
+    }
+
+    /**
+     * Throws error if index is out of bounds
+     */
+    if (index < 0 || index > this.#children.length) {
+      throw new Error('Invalid index');
+    }
+
+    const head = this.#children.slice(0, index);
+    const tail = this.#children.slice(index);
+
+    this.#children = [...head, blockNode, ...tail];
+  }
 }
