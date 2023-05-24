3b3b365f5b67cebc1def2034a90115dd67bbb7d9
Ilya Maroz <moroz.ilya.olegovich@gmail.com>
2023-05-24 21:51:27 +0100

    refactor(editor-document): remove duplicated code from test-cases

--- src/entities/EditorDocument/EditorDocument.spec.ts
+++ src/entities/EditorDocument/EditorDocument.spec.ts
@@ -1,15 +1,6 @@
 import { EditorDocument } from './index';
 import { BlockNode, BlockNodeName, createBlockNodeName } from '../BlockNode';
 
-const createDocument = (): EditorDocument => {
-  return new EditorDocument({
-    children: [],
-    properties: {
-      readOnly: false,
-    },
-  });
-};
-
 const createBlock = ({ name, parent }: { name?: BlockNodeName, parent: EditorDocument }): BlockNode => {
   return new BlockNode({
     name: name || createBlockNodeName('block'),
@@ -19,227 +10,121 @@ const createBlock = ({ name, parent }: { name?: BlockNodeName, parent: EditorDoc
 };
 
 describe('EditorDocument', () => {
+  let document: EditorDocument;
+  let blocksInDocument = 0;
+
+  beforeEach(() => {
+    document = new EditorDocument({
+      children: [],
+      properties: {
+        readOnly: false,
+      },
+    });
+
+    const block1 = createBlock({
+      name: createBlockNodeName('block1'),
+      parent: document,
+    });
+    const block2 = createBlock({
+      name: createBlockNodeName('block2'),
+      parent: document,
+    });
+    const block3 = createBlock({
+      name: createBlockNodeName('block3'),
+      parent: document,
+    });
+
+    document.addBlock(block1);
+    document.addBlock(block2);
+    document.addBlock(block3);
+
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers -- 3 is the number of blocks in the document before each test
+    blocksInDocument = 3;
+  });
+
   describe('addBlock', () => {
-    describe('when index is not provided', () => {
-      let document: EditorDocument;
-      let blocksInDocument = 0;
-
-      beforeEach(() => {
-        document = createDocument();
-
-        const block1 = createBlock({
-          name: createBlockNodeName('block1'),
-          parent: document,
-        });
-        const block2 = createBlock({
-          name: createBlockNodeName('block2'),
-          parent: document,
-        });
-
-        document.addBlock(block1);
-        document.addBlock(block2);
-        blocksInDocument = 2;
+    it('should add the block to the end of the document if index is not provided', () => {
+      // Arrange
+      const block = createBlock({
+        parent: document,
       });
 
-      it('should add the block to the end of the document', () => {
-        // Arrange
-        const block = createBlock({
-          parent: document,
-        });
+      // Act
+      document.addBlock(block);
 
-        // Act
-        document.addBlock(block);
-
-        // Assert
-        expect(document.getBlock(blocksInDocument)).toBe(block);
-      });
+      // Assert
+      expect(document.getBlock(blocksInDocument)).toBe(block);
     });
 
-    describe('when index is 0', () => {
-      let document: EditorDocument;
-
-      beforeEach(() => {
-        document = createDocument();
-
-        const block1 = createBlock({
-          name: createBlockNodeName('block1'),
-          parent: document,
-        });
-        const block2 = createBlock({
-          name: createBlockNodeName('block2'),
-          parent: document,
-        });
-
-        document.addBlock(block1);
-        document.addBlock(block2);
+    it('should add the block to the beginning of the document if index is 0', () => {
+      // Arrange
+      const block = createBlock({
+        name: createBlockNodeName('block'),
+        parent: document,
       });
 
-      it('should add the block to the beginning of the document', () => {
-        // Arrange
-        const block = createBlock({
-          name: createBlockNodeName('block'),
-          parent: document,
-        });
+      // Act
+      document.addBlock(block, 0);
 
-        // Act
-        document.addBlock(block, 0);
-
-        // Assert
-        expect(document.getBlock(0)).toBe(block);
-      });
+      // Assert
+      expect(document.getBlock(0)).toBe(block);
     });
 
-    describe('when index is between 0 and the length of the document', () => {
-      let document: EditorDocument;
-
-      beforeEach(() => {
-        document = createDocument();
-
-        const block1 = createBlock({
-          name: createBlockNodeName('block1'),
-          parent: document,
-        });
-        const block2 = createBlock({
-          name: createBlockNodeName('block2'),
-          parent: document,
-        });
-        const block3 = createBlock({
-          name: createBlockNodeName('block3'),
-          parent: document,
-        });
-
-        document.addBlock(block1);
-        document.addBlock(block2);
-        document.addBlock(block3);
+    it('should add the block to the specified index', () => {
+      // Arrange
+      const block = createBlock({
+        name: createBlockNodeName('block'),
+        parent: document,
       });
 
-      it('should add the block to the specified index', () => {
-        // Arrange
-        const block = createBlock({
-          name: createBlockNodeName('block'),
-          parent: document,
-        });
+      // Act
+      document.addBlock(block, 1);
 
-        // Act
-        document.addBlock(block, 1);
-
-        // Assert
-        expect(document.getBlock(1)).toBe(block);
-      });
+      // Assert
+      expect(document.getBlock(1)).toBe(block);
     });
 
-    describe('when index is equal to the length of the document', () => {
-      let document: EditorDocument;
-      let blocksInDocument = 0;
-
-      beforeEach(() => {
-        document = createDocument();
-
-        const block1 = createBlock({
-          name: createBlockNodeName('block1'),
-          parent: document,
-        });
-        const block2 = createBlock({
-          name: createBlockNodeName('block2'),
-          parent: document,
-        });
-
-        document.addBlock(block1);
-        document.addBlock(block2);
-        blocksInDocument = 2;
+    it('should add the block to the end of the document', () => {
+      // Arrange
+      const block = createBlock({
+        parent: document,
       });
 
-      it('should add the block to the end of the document', () => {
-        // Arrange
-        const block = createBlock({
-          parent: document,
-        });
+      // Act
+      document.addBlock(block, blocksInDocument);
 
-        // Act
-        document.addBlock(block, blocksInDocument);
-
-        // Assert
-        expect(document.getBlock(blocksInDocument)).toBe(block);
-      });
+      // Assert
+      expect(document.getBlock(blocksInDocument)).toBe(block);
     });
 
-    describe('when index is out of bounce of the document block nodes', () => {
-      let document: EditorDocument;
-      let blocksInDocument = 0;
-
-      beforeEach(() => {
-        document = createDocument();
-
-        const block1 = createBlock({
-          name: createBlockNodeName('block1'),
-          parent: document,
-        });
-        const block2 = createBlock({
-          name: createBlockNodeName('block2'),
-          parent: document,
-        });
-
-        document.addBlock(block1);
-        document.addBlock(block2);
-        blocksInDocument = 2;
+    it('should throw an error if index is greater then block nodes length', () => {
+      // Arrange
+      const block = createBlock({
+        parent: document,
       });
 
-      it('should throw an error if index is greater then block nodes length', () => {
-        // Arrange
-        const block = createBlock({
-          parent: document,
-        });
+      // Act
+      const action = (): void | never => document.addBlock(block, blocksInDocument + 1);
 
-        // Act
-        const action = (): void | never => document.addBlock(block, blocksInDocument + 1);
+      // Assert
+      expect(action).toThrowError('Index out of bounds');
+    });
 
-        // Assert
-        expect(action).toThrowError('Index out of bounds');
+    it('should throw an error if index is less then 0', () => {
+      // Arrange
+      const block = createBlock({
+        parent: document,
       });
 
-      it('should throw an error if index is less then 0', () => {
-        // Arrange
-        const block = createBlock({
-          parent: document,
-        });
+      // Act
+      const action = (): void | never => document.addBlock(block, -1);
 
-        // Act
-        const action = (): void | never => document.addBlock(block, -1);
-
-        // Assert
-        expect(action).toThrowError('Index out of bounds');
-      });
+      // Assert
+      expect(action).toThrowError('Index out of bounds');
     });
   });
 
   describe('removeBlock', () => {
-    let document: EditorDocument;
-    let blocksInDocument = 0;
-
-    beforeEach(() => {
-      document = createDocument();
-
-      const block1 = createBlock({
-        name: createBlockNodeName('block1'),
-        parent: document,
-      });
-      const block2 = createBlock({
-        name: createBlockNodeName('block2'),
-        parent: document,
-      });
-      const block3 = createBlock({
-        name: createBlockNodeName('block3'),
-        parent: document,
-      });
-
-      document.addBlock(block1);
-      document.addBlock(block2);
-      document.addBlock(block3);
-
-      // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-      blocksInDocument = 3;
-    });
-
     it('should remove the block from the beginning of the document', () => {
       // Arrange
       const block = document.getBlock(0);
@@ -288,33 +173,6 @@ describe('EditorDocument', () => {
   });
 
   describe('getBlock', () => {
-    let document: EditorDocument;
-    let blocksInDocument = 0;
-
-    beforeEach(() => {
-      document = createDocument();
-
-      const block1 = createBlock({
-        name: createBlockNodeName('block1'),
-        parent: document,
-      });
-      const block2 = createBlock({
-        name: createBlockNodeName('block2'),
-        parent: document,
-      });
-      const block3 = createBlock({
-        name: createBlockNodeName('block3'),
-        parent: document,
-      });
-
-      document.addBlock(block1);
-      document.addBlock(block2);
-      document.addBlock(block3);
-
-      // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-      blocksInDocument = 3;
-    });
-
     it('should return the block from the specific index', () => {
       // Arrange
       const index = 1;
