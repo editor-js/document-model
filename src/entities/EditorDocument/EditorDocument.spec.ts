c81dde98680a32e1df55f63dd904442d072840e0
Ilya Maroz <moroz.ilya.olegovich@gmail.com>
2023-05-26 12:33:25 +0100

    test(editor-document): use named function for creating block node

--- src/entities/EditorDocument/EditorDocument.spec.ts
+++ src/entities/EditorDocument/EditorDocument.spec.ts
@@ -1,13 +1,20 @@
 import { EditorDocument } from './index';
 import { BlockNode, BlockNodeName, createBlockNodeName } from '../BlockNode';
 
-const createBlock = ({ name, parent }: { name?: BlockNodeName, parent: EditorDocument }): BlockNode => {
+/**
+ * Creates a BlockNode with the specified name and adds it to the specified parent.
+ *
+ * @param args - The arguments to pass to the BlockNode constructor.
+ * @param args.name - The name of the BlockNode.
+ * @param args.parent - The parent of the BlockNode.
+ */
+function createBlock({ name, parent }: { name?: BlockNodeName, parent: EditorDocument }): BlockNode {
   return new BlockNode({
     name: name || createBlockNodeName('block'),
     parent,
     children: {},
   });
-};
+}
 
 describe('EditorDocument', () => {
   let document: EditorDocument;
