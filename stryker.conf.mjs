978a9b69e00b791d41a004cebd49720ad1fb60f3
Ilya Maroz <moroz.ilya.olegovich@gmail.com>
2023-06-01 17:02:45 +0100

    feat(stryker): configure minimum coverage

--- stryker.conf.mjs
+++ stryker.conf.mjs
@@ -4,10 +4,22 @@ const config = {
   _comment:
     "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
   packageManager: "yarn",
-  reporters: ["html", "clear-text", "progress"],
+  thresholds: {
+    break: 75,
+  },
+  thresholds_comment: "Minimum required coverage. Increase once we're closer to 100%.",
+  clearTextReporter: {
+    allowEmojis: true,
+  },
+  reporters: [
+    "html",
+    "clear-text",
+    "progress",
+  ],
   testRunner: "jest",
   testRunner_comment:
     "Take a look at https://stryker-mutator.io/docs/stryker-js/jest-runner for information about the jest plugin.",
   coverageAnalysis: "perTest",
 };
+
 export default config;
