4c4d1295c776194fa787d37a0e7f7ff66d71afbb
George Berezhnoy <gohabereg@users.noreply.github.com>
2023-07-03 12:49:38 +0100

    Update src/entities/FormattingNode/index.ts
    Co-authored-by: Ilya Maroz <37909603+ilyamore88@users.noreply.github.com>

--- src/entities/FormattingNode/index.ts
+++ src/entities/FormattingNode/index.ts
@@ -24,7 +24,7 @@ export class FormattingNode implements InlineNode {
   #tool: InlineToolName;
 
   /**
-   * ny additional data associated with the formatting tool
+   * Any additional data associated with the formatting tool
    */
   #data?: InlineToolData;
 
