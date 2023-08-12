5903329b49d05250118841e2228a0f0978e31d4a
Ilya Maroz <moroz.ilya.olegovich@gmail.com>
2023-08-12 12:05:01 +0100

    test(block-node): refactor test cases

--- src/entities/BlockNode/BlockNode.spec.ts
+++ src/entities/BlockNode/BlockNode.spec.ts
@@ -1,65 +1,91 @@
-import { createBlockNodeMock } from '../../utils/mocks/createBlockNodeMock';
-import { createBlockTuneMock } from '../../utils/mocks/createBlockTuneMock';
-import type { EditorDocument } from '../EditorDocument';
-import type { BlockNode } from './index';
-import { createBlockTuneName } from '../BlockTune';
+import { BlockNode } from './index';
 import { createBlockNodeName, createDataKey } from './types';
+
+import { BlockTune, BlockTuneSerialized, createBlockTuneName } from '../BlockTune';
 import { TextNode } from '../TextNode';
-import { createValueNodeMock } from '../../utils/mocks/createValueNodeMock';
+import { ValueNode } from '../ValueNode';
+
+import type { EditorDocument } from '../EditorDocument';
+import type { BlockTuneConstructorParameters } from '../BlockTune/types';
+import type { TextNodeConstructorParameters } from '../TextNode/types';
+import type { ValueNodeConstructorParameters } from '../ValueNode';
 
 describe('BlockNode', () => {
   describe('.serialized', () => {
-    it('should return serialized object representing the BlockNode', () => {
-      // Arrange
-      const tune = createBlockTuneMock({
-        name: createBlockTuneName('styling'),
-        data: {
-          background: 'transparent',
-        },
+    it('should return a name of a tool that created a BlockNode', () => {
+      const blockNodeName = createBlockNodeName('paragraph');
+
+      const blockNode = new BlockNode({
+        name: blockNodeName,
+        data: {},
+        parent: {} as EditorDocument,
       });
 
-      const textNodeDataKey = createDataKey('data-key-1a2b');
-      const textNode = new TextNode({
-        value: 'Hello world!',
-        parent: {} as BlockNode,
+      const serialized = blockNode.serialized;
+
+      expect(serialized.name).toEqual(blockNodeName);
+    });
+
+    it('should call .serialized getter of all tunes associated with the BlockNode', () => {
+      const blockTunesNames = [
+        createBlockTuneName('align'),
+        createBlockTuneName('font-size'),
+        createBlockTuneName('font-weight'),
+      ];
+
+      const spy = jest
+        .spyOn(BlockTune.prototype, 'serialized', 'get')
+        .mockImplementation(() => ({} as BlockTuneSerialized));
+
+      const blockNode = new BlockNode({
+        name: createBlockNodeName('paragraph'),
+        data: {},
+        parent: {} as EditorDocument,
+        tunes: blockTunesNames.reduce((acc, name) => ({
+          ...acc,
+          [name]: new BlockTune({} as BlockTuneConstructorParameters),
+        }), {}),
       });
 
-      const valueNodeDataKey = createDataKey('data-key-3c4d');
-      const valueNode = createValueNodeMock({
-        value: 'https://codex.so',
-      });
+      blockNode.serialized;
 
-      const blockNode = createBlockNodeMock({
+      expect(spy).toHaveBeenCalledTimes(blockTunesNames.length);
+    });
+
+    it('should call .serialized getter of all nodes associated with the BlockNode', () => {
+      const textNodeSerializedSpy = jest
+        .spyOn(TextNode.prototype, 'serialized', 'get')
+        .mockImplementation(() => ({}));
+
+      const valueNodeSerializedSpy = jest
+        .spyOn(ValueNode.prototype, 'serialized', 'get')
+        .mockImplementation(() => ({}));
+
+      const countOfTextNodes = 3;
+      const countOfValueNodes = 1;
+
+      const textNodes = [ ...Array(countOfTextNodes).keys() ]
+        .map((index) => new TextNode({ value: `${index}` } as TextNodeConstructorParameters));
+
+      const valueNodes = [ ...Array(countOfValueNodes).keys() ]
+        .reduce((acc, index) => ({
+          ...acc,
+          [createDataKey(`data-key-${index}c${index}d`)]: new ValueNode({} as ValueNodeConstructorParameters),
+        }), {});
+
+      const blockNode = new BlockNode({
         name: createBlockNodeName('paragraph'),
         data: {
-          [textNodeDataKey]: [ textNode ],
-          [valueNodeDataKey]: valueNode,
-        },
-        tunes: {
-          [createBlockTuneName('styling')]: tune,
+          [createDataKey('data-key-1a2b')]: textNodes,
+          ...valueNodes,
         },
         parent: {} as EditorDocument,
       });
 
-      // Act
-      const serialized = blockNode.serialized;
+      blockNode.serialized;
 
-      // Assert
-      expect(serialized).toEqual({
-        name: 'paragraph',
-        data: {
-          [textNodeDataKey]: [ textNode.serialized ],
-          [valueNodeDataKey]: valueNode.serialized,
-        },
-        tunes: {
-          'styling': {
-            name: 'styling',
-            data: {
-              background: 'transparent',
-            },
-          },
-        },
-      });
+      expect(textNodeSerializedSpy).toHaveBeenCalledTimes(countOfTextNodes);
+      expect(valueNodeSerializedSpy).toHaveBeenCalledTimes(countOfValueNodes);
     });
   });
 });
