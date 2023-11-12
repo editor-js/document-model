1cf80f49a000a22f4be3dcd286492dd63282d993
Ilya Maroz <37909603+ilyamore88@users.noreply.github.com>
2023-11-12 20:52:43 +0000

    chore(indexation): add docs

--- src/utils/EventBus/types/indexation.ts
+++ src/utils/EventBus/types/indexation.ts
@@ -1,6 +1,19 @@
+/**
+ * Alias for a document id
+ */
 type DocumentId = string;
+
+/**
+ * Alias for a block id
+ */
 type BlockId = string;
 
+/**
+ * Index for a block node
+ */
 export type BlockIndex = `${DocumentId}:${BlockId}`;
 
+/**
+ * Possible index types
+ */
 export type Index = BlockIndex;
