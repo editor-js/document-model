dfeacee361729a6a75318b1116182e2b2e1f915b
Peter Savchenko <specc.dev@gmail.com>
2023-11-13 01:40:42 +0300

    feat(playgorund): playground base setup (#45)
    * feat(playgorund): playground base setup
    
    * Update yarn.lock
    
    * fix eslint in root

---
+++ playground/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: [
+    'codex/ts',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.json',
+      './tsconfig.eslint.json',
+    ],
+    ecmaVersion: 2022,
+    extraFileExtensions: [ '.vue' ],
+  },
+  parser: 'vue-eslint-parser',
+  rules: {
+  },
+};
---
+++ playground/README.md
@@ -0,0 +1,10 @@
+# Editor.js Document Playground
+
+Goals of this app is to help to understand the Document Model by visualization and interaction examples
+
+## Usage
+
+```
+yarn install
+yarn dev
+```
