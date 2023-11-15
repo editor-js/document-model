34db498863dfc5f0bc80cf353e4dec6bd9907386
George Berezhnoy <gohabereg@users.noreply.github.com>
2023-11-15 17:16:09 +0000

    Add JSDoc for indexes

--- packages/model/src/utils/EventBus/types/indexing.ts
+++ packages/model/src/utils/EventBus/types/indexing.ts
@@ -21,16 +21,37 @@ export type DocumentIndex = DocumentId;
  */
 export type BlockIndex = [BlockIndexAlias];
 
+/**
+ * Index for a text range
+ */
 export type TextRange = [number, number];
 
+/**
+ * Generic type for BlockNode elements
+ */
 type DataIndex<Type extends 'data' | 'tune'> = [`${Type}@${Type extends 'data' ? DataKey : BlockTuneName}`, ...Partial<BlockIndex>];
 
+/**
+ * TextNode index. It consists of a range of characters and an optional index of a block node
+ */
 export type TextIndex = [...TextRange, ...Partial<DataIndex<'data'>>];
 
+/**
+ * ValueNode index
+ */
 export type ValueIndex = Partial<DataIndex<'data'>>;
 
+/**
+ * TuneNode index. It consists of a tune name and an optional index of a block node
+ */
 export type TuneIndex = [string, ...Partial<DataIndex<'tune'>>];
 
+/**
+ * PropertyNode index
+ */
 export type PropertyIndex = [string, 'property'];
 
+/**
+ * All possible indexes
+ */
 export type Index = BlockIndex | TextIndex | ValueIndex | TuneIndex | PropertyIndex;
