cbc60cab7433bb6538f61e07ed2af5985d7521a1
George Berezhnoy <gohabereg@users.noreply.github.com>
2023-11-15 23:17:09 +0000

    Ignore mock dir in tests

--- packages/model/jest.config.ts
+++ packages/model/jest.config.ts
@@ -4,7 +4,7 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [ '<rootDir>/src/**/*.spec.ts' ],
-  modulePathIgnorePatterns: [ '<rootDir>/.*/__mocks__' ],
+  modulePathIgnorePatterns: [ '<rootDir>/.*/__mocks__', '<rootDir>/.*/mocks' ],
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
