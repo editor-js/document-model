a5e9842706945f642b1353181dd639438c78b517
Ilya Maroz <37909603+ilyamore88@users.noreply.github.com>
2023-11-16 01:05:41 +0000

    feat(dom-adapters): initialise package (#56)
    * feat(dom-adapters): initialise package
    
    * fix(dom-adapters): add reference
    
    * fix(ci): run linter only for model

---
+++ packages/dom-adapters/jest.config.ts
@@ -0,0 +1,22 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [ '<rootDir>/src/**/*.spec.ts' ],
+  modulePathIgnorePatterns: [ '<rootDir>/.*/__mocks__' ],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+} as JestConfigWithTsJest;
---
+++ packages/dom-adapters/stryker.conf.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
+  packageManager: "yarn",
+  thresholds: {
+    break: 75,
+  },
+  thresholds_comment: "Minimum required coverage. Increase once we're closer to 100%.",
+  clearTextReporter: {
+    allowEmojis: true,
+  },
+  reporters: [
+    "html",
+    "clear-text",
+    "progress",
+    "dashboard",
+  ],
+  testRunner: "jest",
+  testRunner_comment:
+    "Take a look at https://stryker-mutator.io/docs/stryker-js/jest-runner for information about the jest plugin.",
+  coverageAnalysis: "perTest",
+  tsconfigFile: "tsconfig.json",
+  checkers: ["typescript"],
+  timeoutMS: 10000,
+  mutate: ["./src/**/*.ts", "!./src/**/__mocks__/*.ts", "!./src/**/*.spec.ts"],
+  /*
+   * In some cases PRs might not have any unit-tests
+   */
+  allowEmpty: true,
+};
+
+export default config;
