8fbf0bfa48473327ea09e633458c891f98528f82
Ilya Maroz <37909603+ilyamore88@users.noreply.github.com>
2024-03-10 15:43:34 +0000

    fix(page-loading): add waitForLoadState to prevent flaky tests

--- packages/playground/tests/example.spec.ts
+++ packages/playground/tests/example.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
+  await page.waitForLoadState(undefined, { timeout: 5000 });
 });
 
 test('has title', async ({ page }) => {
