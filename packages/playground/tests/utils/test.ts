c99717f515b85f866b4bfe3cc355abd7a9a60736
Ilya Maroz <37909603+ilyamore88@users.noreply.github.com>
2024-08-29 00:39:23 +0100

    docs(playwright): add comment to extended test function

--- packages/playground/tests/utils/test.ts
+++ packages/playground/tests/utils/test.ts
@@ -2,7 +2,7 @@ import { test as base  } from '@playwright/test';
 import { PlaygroundPage } from '../fixtures/PlaygroundPage';
 
 /**
- *
+ * Custom test wrapper to extend the base test with the playground page fixture
  */
 export const test = base.extend<{ playgroundPage: PlaygroundPage }>({
   playgroundPage: async ({ page }, use) => {
