33a04bc72225a4896c9bbbd602fd15e372de5e52
George Berezhnoy <gohabereg@users.noreply.github.com>
2024-08-30 00:43:34 +0300

    Update packages/core/src/entities/UnifiedToolConfig.ts
    Co-authored-by: Peter <specc.dev@gmail.com>

--- packages/core/src/entities/UnifiedToolConfig.ts
+++ packages/core/src/entities/UnifiedToolConfig.ts
@@ -1,6 +1,17 @@
 import type { ToolSettings, ToolConstructable } from '@editorjs/editorjs';
 import type { BlockToolConstructor, InlineToolConstructor } from '@editorjs/sdk';
 
+/**
+ * Users can pass tool's config in two ways:
+ *   toolName: ToolClass
+ *   or
+ *   toolName: { 
+ *     class: ToolClass,
+ *     // .. other options      
+ *   }   
+ *
+ * This interface unifies these variants to a single format
+ */
 export type UnifiedToolConfig = Record<string, Omit<ToolSettings, 'class'> & {
   /**
    * Tool constructor
