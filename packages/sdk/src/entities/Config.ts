3c4bb064087d246c0c5256d0aa4386475d36f481
Peter Savchenko <specc.dev@gmail.com>
2024-08-31 01:21:07 +0300

    Update Config.ts

--- packages/sdk/src/entities/Config.ts
+++ packages/sdk/src/entities/Config.ts
@@ -1,4 +1,5 @@
 import type { EditorConfig } from '@editorjs/editorjs';
+import type { EditorJSModel } from '@editorjs/model';
 
 /**
  * Editor.js configuration
@@ -8,4 +9,12 @@ export interface CoreConfig extends EditorConfig {
    * Element to insert the editor into. By default #editorjs
    */
   holder?: HTMLElement;
+
+  /**
+   * DEV MODE ONLY
+   * 
+   * Allows to subscribe to model updates. Used in playground for visualizing model changes
+   * @param model - EditorJSModel instance
+   */
+  onModelUpdate?: (model: EditorJSModel) => void;
 }
