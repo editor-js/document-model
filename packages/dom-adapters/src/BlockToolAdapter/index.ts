8dfd733382e1ba5b6c4ba4fa9fa9fe24a2c6b5fd
e11sy <130844513+e11sy@users.noreply.github.com>
2025-04-10 16:14:13 +0300

    imp(dom-adapters): save formatting on block split (#105)
    * imp(dom-adapters): save formatting on block split
    
    * imp(dom-adapter): cover case with different inputs
    
    * chore(dom-adapters): lint fix
    
    * Update packages/dom-adapters/src/BlockToolAdapter/index.ts
    
    Co-authored-by: George Berezhnoy <gohabereg@users.noreply.github.com>
    
    * imp(dom-adapters): description
    
    * chore(dom-adapters): fix
    
    ---------
    
    Co-authored-by: George Berezhnoy <gohabereg@users.noreply.github.com>

--- packages/dom-adapters/src/BlockToolAdapter/index.ts
+++ packages/dom-adapters/src/BlockToolAdapter/index.ts
@@ -359,6 +359,16 @@ export class BlockToolAdapter implements BlockToolAdapterInterface {
     const currentValue = this.#model.getText(this.#blockIndex, key);
     const newValueAfter = currentValue.slice(end);
 
+    const relatedFragments = this.#model.getFragments(this.#blockIndex, key, end, currentValue.length);
+
+    /**
+     * Fragment ranges bounds should be decreased by end index, because end is the index of the first character of the new block
+     */
+    relatedFragments.forEach(fragment => {
+      fragment.range[0] = Math.max(0, fragment.range[0] - end);
+      fragment.range[1] -= end;
+    });
+
     this.#model.removeText(this.#config.userId, this.#blockIndex, key, start, currentValue.length);
     this.#model.addBlock(
       this.#config.userId,
@@ -368,7 +378,7 @@ export class BlockToolAdapter implements BlockToolAdapterInterface {
           [key]: {
             $t: 't',
             value: newValueAfter,
-            fragments: [],
+            fragments: relatedFragments,
           },
         },
       },
