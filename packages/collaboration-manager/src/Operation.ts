be01020a36389b1c8ad3c63eb0464d469b6438c8
e11sy <130844513+e11sy@users.noreply.github.com>
2025-04-12 01:06:42 +0200

    feat(collab): add operations transformer class

--- packages/collaboration-manager/src/Operation.ts
+++ packages/collaboration-manager/src/Operation.ts
@@ -1,4 +1,5 @@
 import { IndexBuilder, type Index, type BlockNodeSerialized } from '@editorjs/model';
+import { OperationsTransformer } from './OperationsTransformer';
 
 /**
  * Type of the operation
@@ -127,6 +128,11 @@ export class Operation<T extends OperationType = OperationType> {
   public rev?: number;
 
   /**
+   * Transformer for operations
+   */
+  #transformer: OperationsTransformer = new OperationsTransformer();
+
+  /**
    * Creates an instance of Operation
    *
    * @param type - operation type
@@ -215,217 +221,7 @@ export class Operation<T extends OperationType = OperationType> {
    * @param againstOp - operation to transform against
    */
   public transform<K extends OperationType>(againstOp: Operation<K> | Operation<OperationType.Neutral>): Operation<T | OperationType.Neutral> {
-    /**
-     * Do not transform operations if they are on different documents
-     */
-    if (this.index.documentId !== againstOp.index.documentId) {
-      return this;
-    }
-
-    /**
-     * Do not transform if the againstOp index is greater or if againstOp is Modify op
-     */
-    if (!this.#shouldTransform(againstOp.index) || againstOp.type === OperationType.Modify) {
-      return this;
-    }
-
-    const newIndexBuilder = new IndexBuilder().from(this.index);
-
-    switch (true) {
-      /**
-       * If one of the operations is neutral, return this operation
-       */
-      case (this.type === OperationType.Neutral || againstOp.type === OperationType.Neutral): {
-        return Operation.from(this);
-      }
-      /**
-       * Every operation against modify operation stays the same
-       */
-      case (againstOp.type === OperationType.Modify): {
-        break;
-      }
-      case (this.type === OperationType.Insert && againstOp.type === OperationType.Insert): {
-        /**
-         * Update block index if againstOp is insert of a block
-         */
-        if (againstOp.index.isBlockIndex) {
-          newIndexBuilder.addBlockIndex(this.index.blockIndex!++);
-
-          break;
-        }
-
-        /**
-         * Move current insert to the right on amount on chars, inserted by againstOp 
-         */
-        if (this.index.isTextIndex && againstOp.index.isTextIndex) {
-          const againstOpLength = againstOp.data.payload!.length;
-
-          newIndexBuilder.addTextRange([this.index.textRange![0] + againstOpLength, this.index.textRange![1] + againstOpLength])
-
-          break;
-        }
-      }
-      case (this.type === OperationType.Insert && againstOp.type === OperationType.Delete): {
-        /**
-         * Decrease block index if againstOp is Delete block before current insert
-         */
-        if (againstOp.index.isBlockIndex && this.index.blockIndex! > againstOp.index.blockIndex!) {
-          newIndexBuilder.addBlockIndex(this.index.blockIndex!--);
-
-          break;
-        }
-
-        if (this.index.isTextIndex && againstOp.index.isTextIndex) {
-          /**
-           * Deleted the range on the left of the current insert
-           */
-          if (this.index.textRange![0] > againstOp.index.textRange![1]) {
-            newIndexBuilder.addTextRange([this.index.textRange![0] - againstOp.index.textRange![1], this.index.textRange![1] - againstOp.index.textRange![1]]);
-
-            break;
-          }
-
-          /**
-           * Deleted the range, then trying to insert new text inside of the deleted range
-           * Then insert should be done in the start of the deleted range
-           */
-          if ((this.index.textRange![0] <= againstOp.index.textRange![0]) && (this.index.textRange![0] > againstOp.index.textRange![0])) {
-            newIndexBuilder.addTextRange([againstOp.index.textRange![0], againstOp.index.textRange![0]]);
-          }          
-        }
-
-        break;
-      }
-      case (this.type === OperationType.Modify && againstOp.type === OperationType.Insert): {
-        /**
-         * Increase block index of the modify operation if againstOp insert a block before
-         */
-        if (againstOp.index.isBlockIndex) {
-          newIndexBuilder.addBlockIndex(this.index.blockIndex!++);
-
-          break;
-        }
-
-        /**
-         * Extend modify operation range if againstOp insert a text inside of the modify bounds
-         */
-        if (againstOp.index.textRange![0] < this.index.textRange![0] && againstOp.index.textRange![1] > this.index.textRange![0]) {
-          const againstOpLength = againstOp.index.textRange![1] - againstOp.index.textRange![0];
-
-          newIndexBuilder.addTextRange([this.index.textRange![0], this.index.textRange![1] + againstOpLength]);
-        }
-        break;
-      }
-      case (this.type === OperationType.Modify && againstOp.type === OperationType.Delete): {
-        /**
-         * Decrease block index of the modify operation if againstOp delete a block before
-         */
-        if (againstOp.index.isBlockIndex && this.index.blockIndex! > againstOp.index.blockIndex!) {
-          newIndexBuilder.addBlockIndex(this.index.blockIndex!--);
-
-          break;
-        }
-        
-        /**
-         * Make modify operation neutral if againstOp deleted a block, to apply modify to 
-         */
-        if (againstOp.index.isBlockIndex && this.index.blockIndex! === againstOp.index.blockIndex!) {
-          return new Operation(OperationType.Neutral, this.index, { payload: [] }, this.userId);
-
-        }
-        break;
-      }
-      case (this.type === OperationType.Delete && againstOp.type === OperationType.Insert): {
-        /**
-         * Increase block index if againstOp insert a block before
-         */
-        if (againstOp.index.isBlockIndex) {
-          newIndexBuilder.addBlockIndex(this.index.blockIndex!++);
-
-          break;
-        }
-
-        if (this.index.isTextIndex && againstOp.index.isTextIndex) {
-          const againstOpLength = againstOp.data.payload?.length;
-
-          /**
-           * Extend delete operation range if againstOp insert a text inside of the delete bounds
-           */
-          if ((againstOp.index.textRange![0] > this.index.textRange![0]) && (againstOp.index.textRange![0] < this.index.textRange![1])) {
-            newIndexBuilder.addTextRange([this.index.textRange![0], this.index.textRange![1] + againstOpLength]);
-
-            break;
-          }
-
-          /**
-           * Move deletion bounds to the right by amount of inserted text
-           */
-          if (this.index.textRange![0] > againstOp.index.textRange![0]) {
-            newIndexBuilder.addTextRange([this.index.textRange![0] + againstOpLength, this.index.textRange![1] + againstOpLength]);
-          }
-        }
-        break;
-      }
-      case (this.type === OperationType.Delete && againstOp.type === OperationType.Delete): {
-        /**
-         * Decrease block index if againstOp delete a block before
-         */
-        if (againstOp.index.isBlockIndex && this.index.blockIndex! > againstOp.index.blockIndex!) {
-          newIndexBuilder.addBlockIndex(this.index.blockIndex!--);
-
-          break;
-        }
-        
-        if (this.index.isTextIndex && againstOp.index.isTextIndex) {
-          const againstOpLength = againstOp.index.textRange![1] - againstOp.index.textRange![0];
-
-          /**
-           * Move deletion bounds to the left by amount of deleted text
-           */
-          if (this.index.textRange![0] > againstOp.index.textRange![1]) {
-            newIndexBuilder.addTextRange([this.index.textRange![0] - againstOpLength, this.index.textRange![1] - againstOpLength]);
-
-            break;
-          }
-
-          /**
-           * If operation tries to delete a range that is already deleted, return neutral operation
-           */
-          if (this.index.textRange![0] >= againstOp.index.textRange![0] && this.index.textRange![1] <= againstOp.index.textRange![1]) {
-            return new Operation(OperationType.Neutral, this.index, { payload: [] }, this.userId);
-          }
-
-          /**
-           * Remove part of the delete operation range if it is already deleted by againstOp
-           * Cover three possible overlaps
-           */
-          if (this.index.textRange![0] > againstOp.index.textRange![0] && this.index.textRange![1] > againstOp.index.textRange![1]) {
-            newIndexBuilder.addTextRange([againstOp.index.textRange![1], this.index.textRange![1]]);
-
-            break;
-          }
-          if (this.index.textRange![0] < againstOp.index.textRange![0] && this.index.textRange![1] < againstOp.index.textRange![1]) {
-            newIndexBuilder.addTextRange([this.index.textRange![0], againstOp.index.textRange![0]]);
-
-            break;
-          }
-          if (this.index.textRange![0] < againstOp.index.textRange![0] && this.index.textRange![1] > againstOp.index.textRange![1]) {
-            newIndexBuilder.addTextRange([this.index.textRange![0], againstOp.index.textRange![1] - againstOpLength]);
-
-            break;
-          }
-        }
-      }
-      default: {
-        throw new Error('Unsupported operation type');
-      }
-    }
-
-    const operation = Operation.from(this);
-
-    operation.index = newIndexBuilder.build();
-
-    return operation;
+    return this.#transformer.transform(this, againstOp);
   }
 
   /**
@@ -440,31 +236,4 @@ export class Operation<T extends OperationType = OperationType> {
       rev: this.rev!,
     };
   }
-
-  /**
-   * Checks if operation needs to be transformed:
-   * 1. If relative operation (againstOp) happened in the block before or at the same index of the Block of _this_ operation
-   * 2. If relative operation happened in the same block and same data key and before the text range of _this_ operation
-   *
-   * @param indexToCompare - index of a relative operation
-   */
-  #shouldTransform(indexToCompare: Index): boolean {
-    if (indexToCompare.isBlockIndex && this.index.blockIndex !== undefined) {
-      return indexToCompare.blockIndex! <= this.index.blockIndex;
-    }
-
-    if (indexToCompare.isTextIndex && this.index.isTextIndex) {
-      return indexToCompare.dataKey === this.index.dataKey && indexToCompare.textRange![0] <= this.index.textRange![0];
-    }
-
-    return false;
-  }
-
-  // #removeOverlappingTextRange(range: TextRange, againstRange: TextRange): TextRange {
-  //   if (range[0] <= againstRange[0] && range[1] >= againstRange[0]) {
-  //     return [againstRange[0], range[1]];
-  //   }
-
-  //   return range;
-  // }
 }
