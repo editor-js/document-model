f7e2d7886d043f5743a38da6f9f9bd08a46d4da2
Murod Khaydarov <murod.haydarov@gmail.com>
2026-04-14 23:50:31 +0300

    update vite config

--- packages/external-tools/header/vite.config.ts
+++ packages/external-tools/header/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'EditorHeader',
+      name: 'Header',
       formats: ['es', 'cjs'],
       fileName: 'header',
     },
