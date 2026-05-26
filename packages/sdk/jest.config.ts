600799d8bd07a04639bff058a08abeade96f4924
gohabereg <gohabereg@gmail.com>
2026-05-26 22:13:11 +0100

    Update dependencies

--- packages/sdk/jest.config.ts
+++ packages/sdk/jest.config.ts
@@ -2,9 +2,6 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 export default {
   preset: 'ts-jest',
-  // SDK tests require a browser-like environment ('codex-tooltip' accesses `window`), use jsdom
-  // @todo refactor tooltip package to not use window on the root level
-  testEnvironment: 'jsdom',
   testMatch: [ '<rootDir>/src/**/*.spec.ts' ],
   extensionsToTreatAsEsm: [ '.ts' ],
   moduleNameMapper: {
