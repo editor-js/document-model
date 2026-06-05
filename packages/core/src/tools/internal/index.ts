9c08b986dc3ef5d28e095b535a46604f06d74353
Bettina Steger <hello@bettysteger.com>
2026-06-05 16:53:25 +0200

    feat: move from tools to tunes

--- packages/core/src/tools/internal/index.ts
+++ packages/core/src/tools/internal/index.ts
@@ -2,6 +2,3 @@ export * from './block-tools/paragraph/index.js';
 export * from './inline-tools/bold/index.js';
 export * from './inline-tools/italic/index.js';
 export * from './inline-tools/link/index.js';
-export * from './block-tunes/move-up/index.js';
-export * from './block-tunes/delete-block/index.js';
-export * from './block-tunes/move-down/index.js';
